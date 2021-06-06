481364a23bcc3f9aca41685e9d7a4f89708c49ef
okijulian <30639959+okijulian@users.noreply.github.com>
2021-06-06 17:54:29 -0300

    Add files via upload

---
+++ practicando.py
@@ -0,0 +1,8 @@
+cadena = "Ana sdfg"
+#elimino espacios en blanco y pongo
+cadena_sin_espacios = cadena.replace(' ', '').lower()
+#acá podemos hacer un print si queremos chequear cómo quedó con print(cadena_sin_espacios)
+if cadena_sin_espacios == cadena_sin_espacios[::-1]:
+    print(f'La frase ingresada: "{cadena}", es un palíndromo')
+else:
+    print(f'La frase ingresada: "{cadena}", no es un palíndromo')
