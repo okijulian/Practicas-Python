66556b5c875b03ea1e9f6da292a45d898fdf88fc
okijulian <30639959+okijulian@users.noreply.github.com>
2021-06-06 22:47:40 -0300

    Update mayor_de_lista.py

--- mayor_de_lista.py
+++ mayor_de_lista.py
@@ -1,4 +1,17 @@
 # utilizando  ciclo for con in para   comparar elementos de la lista suponiendo siempre de izquierda a derecha la comparativa 
+
+#usando len para contar elementos de la lista
+miLista = [0, 3, 11, 5, 1, 9, 7, 15, 13]
+mayor = miLista[0]
+
+for i in range(1, len(miLista)):
+   if miLista [i]> mayor:
+        mayor = miLista[i]
+
+print(mayor)
+
+
+#con rojada en vez de len
 miLista = [17, 3, 11, 5, 1, 9, 7, 15, 13]
 mayor = miLista [0]
 
@@ -7,4 +20,3 @@ for i in miLista [1:]:
         mayor = i
 
 print(mayor)
-
