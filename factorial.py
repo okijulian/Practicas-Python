69b9dce167a15c589a91a9473d68257a86906658
okijulian <30639959+okijulian@users.noreply.github.com>
2021-06-17 07:14:11 -0300

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -12,3 +12,16 @@ def factorialFun(n):
 
 for n in range(1, 10): # probando
     print(n, factorialFun(n))
+
+    
+    
+    #metodo corto por recursividad
+def factorialFun(n):
+    if n < 0:
+        return None
+    if n < 2:
+        return 1
+    return n * factorialFun(n - 1)
+
+for n in range(1, 10): # probando
+    print(n, factorialFun(n))
