d94ac8fcc2f45244859e4f3954d80f01846acb5e
okijulian <30639959+okijulian@users.noreply.github.com>
2021-06-16 21:37:18 -0300

    Create factorial.py

---
+++ factorial.py
@@ -0,0 +1,14 @@
+#funcion para hacer el factorial de los numeros 
+def factorialFun(n):
+    if n < 0:
+        return None
+    if n < 2:
+        return 1
+    
+    producto = 1
+    for i in range(2, n + 1):
+        producto *= i
+    return producto
+
+for n in range(1, 10): # probando
+    print(n, factorialFun(n))
